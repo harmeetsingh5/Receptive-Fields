943e24ebd6556be211986f99773e4139a09b984b
PatNolan14 <pnolan9@luc.edu>
2018-05-18 10:28:31 -0500

    Add files via upload
    This one actually works

--- VideoWorking.py
+++ VideoWorking.py
@@ -4,15 +4,16 @@ import pylab as py
 import cv2
 from PIL import Image
 
-vidcap = cv2.VideoCapture('bw_nature.mp4')
+vidcap = cv2.VideoCapture('bw_nature.avi')
 success,image = vidcap.read()
 count = 1
 success = True
 while success:
-  cv2.imwrite("Video_images/%d.tiff" % count, image)    
-  img = Image.open("Video_images/%d.tiff" % count)
+  cv2.imwrite("Video_images/%d.jpg" % count, image)    
+  img = Image.open("Video_images/%d.jpg" % count)
   img = img.resize((512,256))
-  img.save("Video_images/%d.tiff" % count, "TIFF", optimize=True)
+  img = img.convert('L')
+  img.save("Video_images/%d.jpg" % count, optimize=True)
   success, image = vidcap.read()
   count += 1
   if(count > 12):
@@ -20,7 +21,7 @@ while success:
 
 
 
-def collect_natural_patches(num_patches = 100000, patch_width = 8):
+def collect_natural_patches(num_patches = 1000000, patch_width = 8):
   """ collects image patches
   the natural images are from a specific folder of 13 .tiff files"""
 
@@ -31,16 +32,16 @@ def collect_natural_patches(num_patches = 100000, patch_width = 8):
   img_first_try = 0 # the first attempt to take a patch from the image
   patch_cnt = 0 # number of collected patches
   try_cnt = 0 # number of attempted collected patches
-  num_pixels = patch_width * patch_width
+  num_pixels = patch_width * patch_width 
 
   patch_sample = np.zeros([patch_width,patch_width],float)
-  patch = np.zeros([num_pixels,1],float)
+  patch = np.zeros([num_pixels,num_patches],float)
   
   img_patches = np.zeros([num_pixels,num_patches],float)
 
   # change the image sampled from
   nat_img_cnt = 1  
-  image = PIL.Image.open('Video_images/' + str(nat_img_cnt) + '.tiff')
+  image = PIL.Image.open('Video_images/' + str(nat_img_cnt) + '.jpg')
   image = np.asarray(image, 'double').transpose()  
   # normalizing the image
   image -= image.mean()
@@ -54,7 +55,7 @@ def collect_natural_patches(num_patches = 100000, patch_width = 8):
       (patch_cnt - img_first_patch) > num_patches/12:
       # change the image sampled from
       nat_img_cnt += 1
-      image = PIL.Image.open('Video_images/' + str(nat_img_cnt) + '.tiff')
+      image = PIL.Image.open('Video_images/' + str(nat_img_cnt) + '.jpg')
       image = np.asarray(image, 'double').transpose() 
       image -= image.mean()
       image /= image.std()
@@ -73,7 +74,7 @@ def collect_natural_patches(num_patches = 100000, patch_width = 8):
     
     if patch_std > 0.0: # > 0 to remove blank/uninteresting patches for speed
       # create the patch vector     
-      patch = np.reshape(patch_sample, num_pixels)     
+      patch = np.reshape(patch_sample, num_pixels)   
       patch = patch - np.mean(patch)         
       img_patches[:,patch_cnt] = patch.copy()
       patch_cnt += 1
